058c42e1d79e1ebfc39d6a19874d7f7e968d8db3
sneha-sil <snehasil2029@u.northwestern.edu>
2026-09-08 16:28:21 -0500

    Initial commit

---
+++ src/graphpancake/__init__.py
@@ -0,0 +1,5 @@
+"""A Python library for generating quantum molecular graphs of organic molecules from electronic structure theory calculations."""
+
+from graphpancake import *
+
+from ._version import __version__
